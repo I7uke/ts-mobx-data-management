4acca5a12c553d01d9ac3967b36587dd5f4b9edf
Малахов Иван <malakhov.i.v.a.n@mail.ru>
2023-03-17 17:31:15 +0300

    Полная переработка StoreDataSource

--- storeDataManagement.ts
+++ storeDataManagement.ts
@@ -1,183 +1,484 @@
 import cloneDeep from "lodash.clonedeep";
 import {DataWithUuid} from "./type/dataWithUuid";
+import { v4 as uuidv4 } from 'uuid';
+
+type ChangeDataSource<TItem extends DataWithUuid> = {
+    readonly changeType: 'addNewItem' | 'editItem' | 'deleteItem';
+    readonly itemsList: TItem[];
+}
 
 type CallbackApplyFilters<TItem extends DataWithUuid> = (dataList: TItem[]) => TItem[];
+type CallbackChangeDataSource<TItem extends DataWithUuid> = (param: ChangeDataSource<TItem>) => void;
 
 type InitData<TItem extends DataWithUuid> = {
-    readonly dataList?:TItem[];
     readonly filters?: CallbackApplyFilters<TItem>;
 }
 
 /**
  * Хранилище для управления данными
  */
-export class StoreDataManager<TItem extends DataWithUuid> {
-
-    private _dataList: TItem[];
-
-    get dataList(): TItem[] {
-        return cloneDeep(this._dataList);
-    }
-
-
-    //region Данные для управления
+export class StoreDataSource<TItem extends DataWithUuid> {
+    /**
+     * Элементы для внутреннего хранения в формат Hash Tables
+     * @private
+     */
+    private _internalItems: Map<string, TItem>;
 
     /**
-     * Получить все текущие управляемые данные
+     * Слушатели изменений элементов
+     * @private
      */
-    get allDataForManage() {
-        return this._dataForManage.slice();
-    }
+    private _callbacksListenersChangeDataSource: Record<string, CallbackChangeDataSource<TItem>>;
+    private _callbackApplyFilters?: CallbackApplyFilters<TItem>;
 
     /**
-     * Получить текущие управляемые данные
+     * Проверяет наличие uuid у элемента и возвращает корректный элемент
+     * Если uuid не строка или пустая строка будет присвоен новый uuid
+     * Если uuid строка и такого uuid нет не у одного элемента будет оставлен переданный uuid, в противном случае присвоен новый uuid
+     * @param item
+     * @private
      */
-    get dataForManage() {
-        const copyData = this._dataForManage.slice();
+    private _validItem(item: TItem): TItem {
+        const copyItem:TItem = cloneDeep(item);
 
-        if (typeof this._applyFilters === 'function') {
-            // Если есть функция фильтрации, фильтруем данные и отдаем их
-            return this._applyFilters(copyData);
-        }
-
-        // Если функции фильтрации нет, просто отдаем данные
-        return copyData;
-    }
-
-    /**
-     * Установлены данные или нет
-     */
-    get isDataSet(): boolean {
-        return !!this._dataForManage.length;
-    }
-
-    /**
-     * Установить данные для управления
-     * @param dataList
-     */
-    set dataForManage(dataList: DataItem[]) {
-        this._dataForManage = dataList.slice();
-    }
-
-    //endregion
-
-    //region Функция фильтрации данных
-    private _applyFilters?: CallbackApplyFilters<DataItem>;
-
-    /**
-     * Установить функцию фильтрации
-     * @param callbackApplyFilters
-     */
-    public setDataFilters(callbackApplyFilters: CallbackApplyFilters<DataItem>) {
-        this._applyFilters = callbackApplyFilters
-    }
-
-    //endregion
-
-    constructor(initData?: InitDataStoreDataManager<DataItem>) {
-        const initDataForManage: DataItem[] | undefined = initData?.dataForManage;
-
-        this._dataForManage = (Array.isArray(initDataForManage)) ? initDataForManage.slice() : [];
-        this._applyFilters = (typeof initData?.callbackApplyFilters === 'function') ? initData.callbackApplyFilters : undefined;
-    }
-
-    /**
-     * Получить uuid элемента по атрибуту data-uuid HTML элемента
-     * @param element
-     */
-    public getUuidByDataAttributeHTMLElement(element: HTMLElement): string | null {
-        return element.getAttribute('data-id');
-    }
-
-    /**
-     * Найти элемент по атрибуту data-uuid
-     * @param element
-     */
-    public getItemByDataAttributeUuid(element: HTMLElement): DataItem | undefined {
-        const uuid: string | null = this.getUuidByDataAttributeHTMLElement(element);
-
-        if (!uuid) {
-            return undefined;
-        }
-
-        const foundElement = this.getItemByUuid(uuid);
-
-        if (!foundElement) {
-            return undefined;
-        }
-
-        return foundElement;
-    }
-
-    /**
-     * Найти элемент по uuid, отдает КОПИЮ
-     * @param uuid
-     */
-    public getItemByUuid(uuid: string): undefined | DataItem {
-        for (let i = 0; i < this._dataForManage.length; ++i) {
-            if (this._dataForManage[i].uuid === uuid) {
-                return Object.assign({}, this._dataForManage[i]);
+        if (typeof copyItem.uuid !== 'string') {
+            return {
+                ...copyItem,
+                uuid: uuidv4()
             }
         }
+
+        if (!copyItem.uuid) {
+            return {
+                ...copyItem,
+                uuid: uuidv4()
+            }
+        }
+
+        if (this._internalItems.has(copyItem.uuid)) {
+            return {
+                ...copyItem,
+                uuid: uuidv4()
+            }
+        }
+
+        return copyItem;
+    }
+
+    /**
+     * Добавляет новый элемент к общему списку
+     * @param newItem
+     * @private
+     */
+    private _addNewItem(newItem: TItem):TItem {
+        const validNewItem = this._validItem(newItem);
+        this._internalItems.set(validNewItem.uuid, validNewItem);
+        return validNewItem;
+    }
+
+    /**
+     * Редактировать существующий элемент
+     * @param existingItem
+     * @private
+     */
+    private _ediItem(existingItem: TItem): TItem | undefined {
+        const validExistingItem = this._validItem(existingItem);
+
+        if(this._internalItems.has(validExistingItem.uuid)){
+            this._internalItems.set(validExistingItem.uuid, validExistingItem);
+            return validExistingItem;
+        }
+
         return undefined;
     }
 
     /**
-     * Удалить элемент по uuid
-     * Вернет true если удалось удалить
+     * Удалить существующий элемент по uuid
      * @param uuid
+     * @private
      */
-    public deleteItemByUuid(uuid: string): boolean {
-        let deleteItemIndex: number = -1;
-        for (let i = 0; i < this._dataForManage.length; ++i) {
-            if (this._dataForManage[i].uuid === uuid) {
-                deleteItemIndex = i;
-            }
-        }
-
-        if (deleteItemIndex === -1) {
+    private _deleteItemByUuid(uuid: string): boolean {
+        if (typeof uuid !== 'string') {
             return false;
         }
 
-        this._dataForManage.splice(deleteItemIndex, 1);
-        return true;
+        if (!uuid) {
+            return false;
+        }
+
+        return this._internalItems.delete(uuid);
+    }
+
+    /**
+     * Получить элемент по uuid
+     * @param uuid
+     * @private
+     */
+    private _getItemByUuid(uuid: string): undefined | TItem {
+        if(typeof uuid !== 'string'){
+            return undefined;
+        }
+
+        if(!uuid){
+            return undefined;
+        }
+
+        if(this._internalItems.has(uuid)) {
+            return cloneDeep(this._internalItems.get(uuid));
+        }
+
+        return undefined;
+    }
+
+    /**
+     * Преобразовать элементы в массив и получить их
+     * @private
+     */
+    private _getItemsArray(): TItem[] {
+        const arr: TItem[] = Array.from(this._internalItems.values());
+        return cloneDeep(arr);
+    }
+
+    /**
+     * Применить слушатели изменения данных
+     * @param param
+     * @private
+     */
+    private _applyCallbacksListenersChangeDataSource(param: ChangeDataSource<TItem>) {
+        for (const keyListener in this._callbacksListenersChangeDataSource) {
+            if (this._callbacksListenersChangeDataSource.hasOwnProperty(keyListener)) {
+                if (typeof this._callbacksListenersChangeDataSource[keyListener] === 'function') {
+                    this._callbacksListenersChangeDataSource[keyListener](param);
+                }
+            }
+        }
+    }
+
+    private _applyFilters(): TItem[] {
+        const itemsArray: TItem[] = this._getItemsArray();
+
+        if (typeof this._callbackApplyFilters === 'function') {
+            return this._callbackApplyFilters(itemsArray);
+        }
+
+        return itemsArray;
+    }
+
+
+    constructor(initData?: InitData<TItem>) {
+        this._internalItems = new Map<string, TItem>();
+        this._callbacksListenersChangeDataSource = {};
+        this._callbackApplyFilters = initData?.filters;
+    }
+
+    /**
+     * Получить список элементов
+     */
+    get itemsList(): TItem[] {
+        const arr: TItem[] = Array.from(this._internalItems.values());
+        return cloneDeep(arr);
+    }
+
+    //region Добавить/удалить слушатель изменения данных
+    /**
+     * Добавить слушатель изменения данных
+     * @param listener
+     */
+    public addChangeDataSourceListener(listener: CallbackChangeDataSource<TItem>){
+        const listenerId: string = uuidv4();
+        this._callbacksListenersChangeDataSource[listenerId] = listener;
+    }
+
+    /**
+     * Удалить слушатель изменения данных
+     * @param listener
+     */
+    public removeChangeDataSourceListener(listener: CallbackChangeDataSource<TItem>) {
+        for (const listenerId in this._callbacksListenersChangeDataSource) {
+            if (listener === this._callbacksListenersChangeDataSource[listenerId]) {
+                delete this._callbacksListenersChangeDataSource[listenerId];
+                return;
+            }
+        }
+    }
+    //endregion
+
+
+    /**
+     * Получить элемент по его uuid
+     * Если не удалось найти элемент по uuid вернет undefined
+     * @param uuid
+     */
+    public getItemByUuid(uuid: string): undefined | TItem {
+        return this._getItemByUuid(uuid);
+    }
+
+    /**
+     * Получить список элементов по их uuid
+     * @param uuidList
+     */
+    public getItemsListByUuid(uuidList: string[]): TItem[] {
+        const result: TItem[] = [];
+
+        if(!Array.isArray(uuidList)){
+            return result;
+        }
+
+        for(const uuid of uuidList){
+            const item = this._getItemByUuid(uuid);
+            if(item){
+                result.push(item);
+            }
+        }
+
+        return result;
+    }
+
+
+    /**
+     * Удалить существующий элемент по uuid
+     * В случае успешного удаления вернет true
+     * Если не удалось удалить элемент вернет false
+     * @param uuid - uuid элемента который нужно удалить
+     * @param isWithoutTrigger - триггеры слушателей не сработают если флаг будет установлен в true
+     */
+    public deleteItemByUuid(uuid: string, isWithoutTrigger?: boolean ): boolean {
+        const isDeleteItem: boolean = this._deleteItemByUuid(uuid);
+
+        if(isWithoutTrigger) {
+            this._applyCallbacksListenersChangeDataSource({
+                changeType: 'deleteItem',
+                itemsList: this._applyFilters()
+            });
+        }
+
+        return isDeleteItem
+    }
+
+    /**
+     * Удалить список элементов по их uuid
+     * Вернет true если удалось удалить все переданные элементы
+     * Если хотя бы один элемент не удалось удалить вернет false
+     * @param uuidList - список uuid элементов которые нужно удалить
+     * @param isWithoutTrigger - триггеры слушателей не сработают если флаг будет установлен в true
+     */
+    public deleteItemsListByUuid(uuidList: string[], isWithoutTrigger?: boolean): boolean {
+        if (!Array.isArray(uuidList)) {
+            return false;
+        }
+
+        let isDeleteAllSuccess: boolean = true;
+
+        for (const uuid of uuidList) {
+            const isDeleteItemSuccess: boolean = this._deleteItemByUuid(uuid);
+
+            if (!isDeleteItemSuccess) {
+                isDeleteAllSuccess = false;
+            }
+        }
+
+        if (isWithoutTrigger) {
+            if (isDeleteAllSuccess) {
+                this._applyCallbacksListenersChangeDataSource({
+                    changeType: 'deleteItem',
+                    itemsList: this._applyFilters()
+                });
+            }
+        }
+
+        return isDeleteAllSuccess;
     }
 
     /**
      * Добавить новый элемент
-     * @param item
+     * @param item - новый элемент который нужно добавить
+     * @param isWithoutTrigger - триггеры слушателей не сработают если флаг будет установлен в true
      */
-    public addNewItem(item: DataItem): DataItem {
-        const newItemCopy: DataItem = {
-            ...item,
-            uuid: uuidv4()
-        };
-        // Добавляем в начало
-        this._dataForManage.unshift(newItemCopy);
-        return newItemCopy;
-    }
+    public addNewItem(item: TItem, isWithoutTrigger?: boolean ): TItem {
+       const newItem = this._addNewItem(item);
 
-    /**
-     * Редактировать существующий элемент, создает КОПИЮ и редактирует ее
-     * Вернет true если элемент удалось изменить
-     * @param item
-     */
-    public editExistingItem(item: DataItem): DataItem | undefined {
-        const copyEditItem: DataItem = Object.assign({}, item);
-
-        for (let i = 0; i < this._dataForManage.length; ++i) {
-            if (this._dataForManage[i].uuid === copyEditItem.uuid) {
-                this._dataForManage[i] = copyEditItem;
-                return copyEditItem;
-            }
+        if(isWithoutTrigger) {
+            this._applyCallbacksListenersChangeDataSource({
+                changeType: 'addNewItem',
+                itemsList: this._applyFilters()
+            });
         }
-        return undefined;
+
+        return newItem;
     }
 
     /**
-     * Сбросить все данные хранилища
+     * Добавить список новых элементов
+     * @param itemsList - список новых элементов которые необходимо добавить
+     * @param isWithoutTrigger
      */
-    public resetStoreData() {
-        this._dataForManage = [];
+    public addNewItemsList(itemsList: TItem[], isWithoutTrigger?: boolean ): TItem[] {
+        const addedItemsList: TItem[] = [];
+
+        if(!Array.isArray(itemsList)) {
+            return addedItemsList;
+        }
+
+        for(const item of itemsList){
+            const addedItem = this._addNewItem(item);
+            addedItemsList.push(addedItem);
+        }
+
+        if(isWithoutTrigger) {
+            this._applyCallbacksListenersChangeDataSource({
+                changeType: 'addNewItem',
+                itemsList: this._applyFilters()
+            });
+        }
+
+        return addedItemsList;
     }
+
+
+
+
+    //
+    // /**
+    //  * Редактировать существующий элемент, создает КОПИЮ и редактирует ее
+    //  * Вернет true если элемент удалось изменить
+    //  * @param item
+    //  */
+    // public editExistingItem(item: DataItem): DataItem | undefined {
+    //     const copyEditItem: DataItem = Object.assign({}, item);
+    //
+    //     for (let i = 0; i < this._dataForManage.length; ++i) {
+    //         if (this._dataForManage[i].uuid === copyEditItem.uuid) {
+    //             this._dataForManage[i] = copyEditItem;
+    //             return copyEditItem;
+    //         }
+    //     }
+    //     return undefined;
+    // }
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    // /**
+    //  * Сбросить все данные хранилища
+    //  */
+    // public resetStoreData() {
+    //     this._dataForManage = [];
+    // }
+    //
+    //
+    //
+    //
+    // //region Данные для управления
+    //
+    // /**
+    //  * Получить все текущие управляемые данные
+    //  */
+    // get allDataForManage() {
+    //     return this._dataForManage.slice();
+    // }
+    //
+    // /**
+    //  * Получить текущие управляемые данные
+    //  */
+    // get dataForManage() {
+    //     const copyData = this._dataForManage.slice();
+    //
+    //     if (typeof this._applyFilters === 'function') {
+    //         // Если есть функция фильтрации, фильтруем данные и отдаем их
+    //         return this._applyFilters(copyData);
+    //     }
+    //
+    //     // Если функции фильтрации нет, просто отдаем данные
+    //     return copyData;
+    // }
+    //
+    // /**
+    //  * Установлены данные или нет
+    //  */
+    // get isDataSet(): boolean {
+    //     return !!this._dataForManage.length;
+    // }
+    //
+    // /**
+    //  * Установить данные для управления
+    //  * @param dataList
+    //  */
+    // set dataForManage(dataList: DataItem[]) {
+    //     this._dataForManage = dataList.slice();
+    // }
+    //
+    // //endregion
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    // //region Функция фильтрации данных
+    // private _applyFilters?: CallbackApplyFilters<DataItem>;
+    //
+    // /**
+    //  * Установить функцию фильтрации
+    //  * @param callbackApplyFilters
+    //  */
+    // public setDataFilters(callbackApplyFilters: CallbackApplyFilters<DataItem>) {
+    //     this._applyFilters = callbackApplyFilters
+    // }
+    //
+    // //endregion
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    // /**
+    //  * Получить uuid элемента по атрибуту data-uuid HTML элемента
+    //  * @param element
+    //  */
+    // public getUuidByDataAttributeHTMLElement(element: HTMLElement): string | null {
+    //     return element.getAttribute('data-id');
+    // }
+    //
+    // /**
+    //  * Найти элемент по атрибуту data-uuid
+    //  * @param element
+    //  */
+    // public getItemByDataAttributeUuid(element: HTMLElement): DataItem | undefined {
+    //     const uuid: string | null = this.getUuidByDataAttributeHTMLElement(element);
+    //
+    //     if (!uuid) {
+    //         return undefined;
+    //     }
+    //
+    //     const foundElement = this.getItemByUuid(uuid);
+    //
+    //     if (!foundElement) {
+    //         return undefined;
+    //     }
+    //
+    //     return foundElement;
+    // }
+
+
 }
