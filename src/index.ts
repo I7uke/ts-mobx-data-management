b70aedce6df9799fc45cd08b218d86a5e53846f4
Малахов Иван <malakhov.i.v.a.n@mail.ru>
2023-04-13 17:13:37 +0300

    Изменены имена импортов

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export {default as uniqueUuid} from "./uniqueUuid";
-export {default as storeSitePageTitle} from "./storeSitePageTitle";
-export {default as abstractStorePage} from "./abstractStorePage";
-export {default as abstractStoreEditItemsPageContent} from "./abstractStoreEditItemsPageContent";
-export {default as abstractStoreEditItem} from "./abstractStoreEditItem";
+export {default as UniqueUuid} from "./uniqueUuid";
+export {default as StoreSitePageTitle} from "./storeSitePageTitle";
+export {default as AbstractStorePage} from "./abstractStorePage";
+export {default as AbstractStoreEditItemsPageContent} from "./abstractStoreEditItemsPageContent";
+export {default as AbstractStoreEditItem} from "./abstractStoreEditItem";
 export {default as StoreDataSource} from "./storeDataSource";
 export {default as StoreDisplayedData} from "./storeDisplayedData";
 export {default as AbstractStoreFilters} from "./abstractStoreFilters";
